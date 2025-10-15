f746b12652d20e93ddec1c03dbbedb9c7859d01d
Reed Mullanix <reedmullanix@gmail.com>
2025-10-14 20:31:42 -0400

    test: make 'n' implicit in postulate

--- panbench/generators/src/Panbench/Generator/LargeDependentRecord.hs
+++ panbench/generators/src/Panbench/Generator/LargeDependentRecord.hs
@@ -13,7 +13,7 @@ generator =
   ] \size ->
   [ postulate (["n" .: builtin "Nat"] |- ("P" .: builtin "Type"))
   , postulate ([] |- "nil" .: app "P" [nat 0])
-  , postulate (["n" .: builtin "Nat", "xs" .: app "P" ["n"]] |- ("cons" .: app "P" [parens (op1 "suc" "n")]))
+  , postulate ([implicit ("n" .: builtin "Nat"), "xs" .: app "P" ["n"]] |- ("cons" .: app "P" [parens (op1 "suc" "n")]))
   , record_ ([] |- "Cap_X" .: builtin "Type") "Const" $
       (nameN "f" 1 .: builtin "Nat") :
       [ nameN "f" i .: app "P" [iter (parens . (op1 "suc")) (nameN "f" 1) (i - 1)]
@@ -21,8 +21,9 @@ generator =
       ]
   -- [TODO: Reed M, 26/09/2025] Should we use a record constructor literal here?
   , ([] |- "example" .: "Cap_X") .=
-      app "Const"
-        [ foldr (\j tm -> parens $ app "cons" [nat j, tm]) "nil" [0..i-1]
-        | i <- [1..size]
+      app "Const" $
+        nat 0 :
+        [ foldr (\_ tm -> parens $ app "cons" [tm]) "nil" [0..i-1]
+        | i <- [1..size-1]
         ]
   ]
