81e212664eeebff5f97a75978977e7cc3796624a
Reed Mullanix <reedmullanix@gmail.com>
2025-10-15 10:50:46 -0400

    test: use data instead of postulate for LargeDependentRecord
    We want to be measuring the time it takes to typecheck the record defn anyways

--- panbench/generators/src/Panbench/Generator/LargeDependentRecord.hs
+++ panbench/generators/src/Panbench/Generator/LargeDependentRecord.hs
@@ -1,5 +1,7 @@
 module Panbench.Generator.LargeDependentRecord where
 
+import Prelude hiding (pi)
+
 import Numeric.Natural
 
 import Panbench.Generator
@@ -11,19 +13,21 @@ generator =
   GenModule "Fields_DependentRecordModule"
   [ import_ "Data.Nat"
   ] \size ->
-  [ postulate (["n" .: builtin "Nat"] |- ("P" .: builtin "Type"))
-  , postulate ([] |- "nil" .: app "P" [nat 0])
-  , postulate ([implicit ("n" .: builtin "Nat"), "xs" .: app "P" ["n"]] |- ("cons" .: app "P" [parens (op1 "suc" "n")]))
+  [ data_ ([] |- ("P" .: pi ["n" .: builtin "Nat"] (builtin "Type")))
+    [ "PZ" .: app "P" [nat 0]
+    , "PS" .: pi [implicit ("n" .: builtin "Nat"), "xs" .: app "P" ["n"]] (app "P" [parens (op1 "suc" "n")])
+    ]
   , record_ ([] |- "Cap_X" .: builtin "Type") "Const" $
       (nameN "f" 1 .: builtin "Nat") :
       [ nameN "f" i .: app "P" [iter (parens . (op1 "suc")) (nameN "f" 1) (i - 1)]
       | i <- [2..size]
       ]
   -- [TODO: Reed M, 26/09/2025] Should we use a record constructor literal here?
-  , ([] |- "example" .: "Cap_X") .=
+  , ([] |- "test" .: "Cap_X") .=
       app "Const" $
         nat 0 :
-        [ foldr (\_ tm -> parens $ app "cons" [tm]) "nil" [0..i-1]
+        [ foldr (\_ tm -> parens $ app "PS" [tm]) "PZ" [0..i-1]
         | i <- [1..size-1]
         ]
+
   ]
