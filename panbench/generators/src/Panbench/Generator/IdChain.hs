ff020754fbaf2bf2cb05bc89b02bddd5aecf8d72
Jacques Carette <carette@mcmaster.ca>
2025-11-07 14:39:42 -0500

    raw id chain test, not plugged in yet.

---
+++ panbench/generators/src/Panbench/Generator/IdChain.hs
@@ -0,0 +1,20 @@
+module Panbench.Generator.IdChain where
+
+import Numeric.Natural
+
+import Panbench.Generator
+import Panbench
+
+generator :: (Constant tm "Type",
+  Definition defns lhs tm, TelescopeLhs lhs hd cell,
+  Chk nm tm cell, Chk nm tm hd,
+  Name nm, Implicit cell, Name tm, App tm) => GenModule hdr defns Natural
+generator =
+    GenModule "IdChain"
+      [
+      ] \size ->
+      [ ([implicit ("A" .: builtin "Type"), ("x" .: "A")] |- ("id" .: "A") .= "x")
+      ,
+        [implicit ("A" .: builtin "Type"), "x" .: "A"] |- "test" .: "A" .=
+           foldr (\_ tm -> app "id" [tm]) "id" [1..size]
+      ]
