e8979c2dffc809eef7b09fda6550dd908fd6e216
Jacques Carette <carette@mcmaster.ca>
2025-11-13 13:57:22 -0500

    these are not quite right but closer. will get replaced soon.

--- panbench/generators/test/snapshot/IdChain.v
+++ panbench/generators/test/snapshot/IdChain.v
@@ -3,6 +3,6 @@ Module IdChain.
 
 Definition id {A : Type} (x : A) : A := x.
 
-Definition test {A : Type} (x : A) : A := id id id id id id x.
+Definition test {A : Type} (x : A) : A := id id id id id x.
 
 End IdChain.
