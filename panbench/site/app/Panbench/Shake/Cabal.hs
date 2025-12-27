6707c6a5ab524605660825aa50eca237480878a5
Reed Mullanix <reedmullanix@gmail.com>
2025-12-27 13:18:04 -0500

    feat: pin hackage index in cabal shake helper

--- panbench/site/app/Panbench/Shake/Cabal.hs
+++ panbench/site/app/Panbench/Shake/Cabal.hs
@@ -2,7 +2,9 @@
 module Panbench.Shake.Cabal
   (
     -- * Cabal Commands
-    CabalA(..)
+    CabalQ(..)
+  , CabalA(..)
+  , needCabal
   , cabalCommand
   , cabalCommand_
     -- * Shake Rules
@@ -27,7 +29,9 @@ import System.Directory.OsPath qualified as Dir
 -- Cabal Commands
 
 -- | Find a version of @cabal@ on the system path.
-data CabalQ = CabalQ
+newtype CabalQ = CabalQ
+  { cabalHackageIndex :: String
+  }
   deriving stock (Eq, Ord, Show, Generic)
   deriving anyclass (Hashable, Binary, NFData)
 
@@ -43,26 +47,35 @@ data CabalA = CabalA
   -- ^ Absolute path of the @cabal@ store.
   , cabalConfig :: OsPath
   -- ^ Absolute path of the @cabal@ config.
+  , cabalIndexState :: OsString
   }
   deriving stock (Eq, Ord, Show, Generic)
   deriving anyclass (Hashable, Binary, NFData)
 
 type instance RuleResult CabalQ = CabalA
 
-cabalCommand :: (HasCallStack, CmdResult r) => [CmdOption] -> [String] -> Action r
-cabalCommand opts args = do
-  CabalA{..} <- askOracle CabalQ
-  putInfo $ "# cabal " ++ unwords args
-  quietly $ osCommand opts cabalBinPath (["--config-file=" ++ decodeOS cabalConfig, "--store-dir=" ++ decodeOS cabalStore] ++ args)
+cabalArgs :: CabalA -> [String]
+cabalArgs CabalA{..} =
+  [ "--config-file=" ++ decodeOS cabalConfig
+  , "--store-dir=" ++ decodeOS cabalStore
+  , "--index-state=" ++ decodeOS cabalIndexState
+  ]
 
-cabalCommand_ :: (HasCallStack) => [CmdOption] -> [String] -> Action ()
-cabalCommand_ opts args = do
-  CabalA{..} <- askOracle CabalQ
+-- | Run a @cabal@ command with a particular @cabal@ configuration.
+cabalCommand :: (HasCallStack, CmdResult r) => [CmdOption] -> CabalA -> [String] -> Action r
+cabalCommand opts cabal args = do
   putInfo $ "# cabal " ++ unwords args
-  quietly $ osCommand_ opts cabalBinPath (["--config-file=" ++ decodeOS cabalConfig, "--store-dir=" ++ decodeOS cabalStore] ++ args)
+  quietly $ osCommand opts (cabalBinPath cabal) (cabalArgs cabal ++ args)
 
+-- | Run a @cabal@ command with a particular @cabal@ configuration, and ignore the result.
+cabalCommand_ :: (HasCallStack) => [CmdOption] -> CabalA -> [String] -> Action ()
+cabalCommand_ opts cabal args = do
+  putInfo $ "# cabal " ++ unwords args
+  quietly $ osCommand_ opts (cabalBinPath cabal) (cabalArgs cabal ++ args)
+
+-- | Shake oracle for getting @cabal@, and setting up a sandbox.
 findCabalOracle :: CabalQ -> Action CabalA
-findCabalOracle CabalQ =
+findCabalOracle cabal@CabalQ {..} =
   liftIO (Dir.findExecutable [osp|cabal|]) >>= \case
     Nothing ->
       fail $ unlines
@@ -73,12 +86,17 @@ findCabalOracle CabalQ =
       cwd <- liftIO Dir.getCurrentDirectory
       Stdout cabalVersion <- osCommand [] cabalBinPath ["--numeric-version"]
       cabalDigest <- fileDigest cabalBinPath
-      let cabalConfig = [osp|$cwd/_build/cabal/config|]
-      let cabalStore = [osp|$cwd/_build/cabal/store|]
       -- Set up a sandboxed cabal config and hackage index.
-      osCommand_ [AddEnv "HOME" (decodeOS [osp|$cwd/_build/cabal|])] cabalBinPath ["--config-file=" ++ decodeOS cabalConfig , "user-config", "init", "--force"]
-      osCommand_ [] cabalBinPath ["--config-file=" ++ decodeOS cabalConfig, "update"]
+      let cabalDir = "cabal" <> "-" <> showHex (binaryDigest cabal)
+      let cabalConfig = [osp|$cwd/_build/$cabalDir/config|]
+      let cabalStore = [osp|$cwd/_build/$cabalDir/store|]
+      osCommand_ [AddEnv "HOME" (decodeOS [osp|$cwd/_build/$cabalDir|])] cabalBinPath ["--config-file=" ++ decodeOS cabalConfig , "user-config", "init", "--force"]
+      osCommand_ [] cabalBinPath ["--config-file=" ++ decodeOS cabalConfig, "update", "hackage.haskell.org," ++ cabalHackageIndex]
+      let cabalIndexState = encodeOS cabalHackageIndex
       pure CabalA {..}
+
+needCabal :: CabalQ -> Action CabalA
+needCabal = askOracle
 
 --------------------------------------------------------------------------------
 -- Shake Rules
