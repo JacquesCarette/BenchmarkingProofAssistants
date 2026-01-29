9475c01c5a4a27ad2673319336c79eedcfd4dc80
Reed Mullanix <reedmullanix@gmail.com>
2026-01-29 12:06:31 -0500

    fix: update IdChain so that all systems can typecheck it
    We needed to mark all of the implicits as unbound, use a different
    name then "id" to make lean happy, and then use lowercase type vars
    to make Idris not confused about constructors

--- lib/generators/src/Panbench/Generator/IdChain.hs
+++ lib/generators/src/Panbench/Generator/IdChain.hs
@@ -6,26 +6,25 @@ import Panbench.Generator
 import Panbench
 
 generator :: (
-  Definition lhs tm defns,
-  TelescopeLhs cell hd lhs,
-  Binder Single nm Single tm cell,
-  Binder Single nm Single tm arrCell,
-  Binder Single nm Single tm lamCell,
-  Implicit cell,
-  Binder Single nm Single tm hd,
-  Binder None nm Single tm arrCell,
+  Definition defnLhs tm defns,
+  TelescopeLhs defnCell defnHd defnLhs,
+  Implicit defnCell, Unbound defnCell,
+  Binder Single nm Single tm defnCell,
+  Binder Single nm Single tm defnHd,
   Name nm,
   Name tm,
   App tm,
   Arr arrCell tm,
+  Binder None nm Single tm arrCell,
   Lam lamCell tm,
+  Binder Single nm Single tm lamCell,
   Builtin tm "Type" tm) => GenModule hdr defns Natural
 generator =
     GenModule "IdChain"
       [
       ] \size ->
-      [ ([implicit ("A" .: builtin "Type")] |- ("id" .: ((None .:* "A") `arr` "A")) .= lam ["x" .: "A"] "x")
+      [ [unbound $ implicit ("a" .: builtin "Type")] |- ("f" .: (None .:* "a" `arr` "a")) .= lam ["x" .: "a"] "x"
       ,
-        [implicit ("A" .: builtin "Type")] |- "test" .: ((None .:* "A") `arr` "A") .=
-           foldr (\_ tm -> app "id" [tm]) "id" [1..size]
+        [unbound $ implicit ("a" .: builtin "Type")] |- "test" .: (None .:* "a" `arr` "a") .=
+           foldr (\_ tm -> app "f" [tm]) "f" [1..size]
       ]
